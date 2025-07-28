c733646c7e1cc63fae514e7f7c38db656960c89f
BuelentUendes <buelent.tt@web.de>
2025-07-28 17:22:17 +0200

    updated the bash scripts for the next runs

--- bash_script.sh
+++ bash_script.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
  ## Bash script for running several experiments
- seeds=(1 3 5 7)
+ seeds=(1 3 5 7 42)
  # shellcheck disable=SC2068
  # shellcheck disable=SC1073
  # shellcheck disable=SC1061
 
  for i in "${seeds[@]}"
  do
-   python3 ./training_pipelines/tstcc_train_cleaned.py --seed $i --force_retraining --gpu 0
-   python3 ./training_pipelines/tstcc_train_cleaned.py --seed $i --force_retraining --gpu 0 --pretrain_all_conditions
+   python3 ./training_pipelines/tstcc_soft_train_cleaned.py --seed $i --force_retraining --gpu 0 --pretrain_all_conditions
+   python3 ./training_pipelines/ts2vec_train_cleaned.py --seed $i --force_retraining --gpu 0 --pretrain_all_conditions
+   python3 ./training_pipelines/ts2vec_soft_train_cleaned.py --seed $i --force_retraining --gpu 0 --pretrain_all_conditions
  done
