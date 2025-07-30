e1a0256cc10290093a443807e4aebc4c4627d841
BuelentUendes <buelent.tt@web.de>
2025-07-30 15:16:56 +0200

    added different path here

--- utils/helper_paths.py
+++ utils/helper_paths.py
@@ -6,5 +6,5 @@ from os.path import abspath
 FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 BASE_PATH = abspath(os.path.join(FILE_PATH, './../'))
 DATA_PATH = abspath(os.path.join(BASE_PATH, './', "data"))
-RAW_DATA_PATH = abspath(os.path.join(BASE_PATH, './', "data", "raw"))
+RAW_DATA_PATH = abspath(os.path.join(BASE_PATH, './', "data", "raw", "Raw ECG Project"))
 SAVED_MODELS_PATH = abspath(os.path.join(BASE_PATH, 'saved_models'))
