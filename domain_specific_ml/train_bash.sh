075c49ffed70fa4e68ebdab1707c43d925256e46
BuelentUendes <buelent.tt@web.de>
2025-08-08 12:10:46 +0200

    simple script for mlp for ecg features

---
+++ domain_specific_ml/train_bash.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+ ## Bash script for running several experiments
+fractions=(0.01 0.05 0.1 0.25 0.5 1.0)
+ # shellcheck disable=SC2068
+ # shellcheck disable=SC1073
+ # shellcheck disable=SC1061
+
+ for fraction in "${fractions[@]}"
+ do
+   python3 train_simple_classifiers.py --classifier_model mlp --gpu 0 --pretrain_all_conditions --label_fraction $fraction
+ done
