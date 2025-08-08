5da103ee813d356f976e62e786d731d8ac91d9eb
BuelentUendes <buelent.tt@web.de>
2025-08-08 16:07:04 +0200

    bash script for running supervised cv

---
+++ training_pipelines/train_supervised_cv.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+ ## Bash script for running several experiments
+fractions=(0.01 0.05 0.1 0.25 0.5 1.0)
+ # shellcheck disable=SC2068
+ # shellcheck disable=SC1073
+ # shellcheck disable=SC1061
+
+ for fraction in "${fractions[@]}"
+ do
+   python3 supervised_cleaned_cv.py --model_type cnn --gpu 0 --label_fraction $fraction --batch_size 64 --force_retraining
+   python3 supervised_cleaned_cv.py --model_type tcn --gpu 0 --label_fraction $fraction --batch_size 64 --force_retraining
+   python3 supervised_cleaned_cv.py --model_type transformer --gpu 0 --label_fraction $fraction --batch_size 64 --force_retraining
+ done
