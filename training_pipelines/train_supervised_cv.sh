a89e3c86545bd260dead7e857ede1d05bf089fad
BuelentUendes <buelent.tt@web.de>
2025-08-08 16:09:19 +0200

    bash script for running supervised cv

--- training_pipelines/train_supervised_cv.sh
+++ training_pipelines/train_supervised_cv.sh
@@ -7,7 +7,7 @@ fractions=(0.01 0.05 0.1 0.25 0.5 1.0)
 
  for fraction in "${fractions[@]}"
  do
-   python3 supervised_cleaned_cv.py --model_type cnn --gpu 0 --label_fraction $fraction --batch_size 64 --force_retraining
-   python3 supervised_cleaned_cv.py --model_type tcn --gpu 0 --label_fraction $fraction --batch_size 64 --force_retraining
-   python3 supervised_cleaned_cv.py --model_type transformer --gpu 0 --label_fraction $fraction --batch_size 64 --force_retraining
+   python3 supervised_training_cleaned_cv.py --model_type cnn --gpu 0 --label_fraction $fraction --batch_size 64 --force_retraining
+   python3 supervised_training_cleaned_cv.py --model_type tcn --gpu 0 --label_fraction $fraction --batch_size 64 --force_retraining
+   python3 supervised_training_cleaned_cv.py --model_type transformer --gpu 0 --label_fraction $fraction --batch_size 64 --force_retraining
  done
