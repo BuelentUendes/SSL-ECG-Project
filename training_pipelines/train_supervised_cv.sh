680f124ec7479a61d93cb15fbceba46699ab1c9d
BuelentUendes <buelent.tt@web.de>
2025-08-21 15:32:43 +0200

    added training protocol

--- training_pipelines/train_supervised_cv.sh
+++ training_pipelines/train_supervised_cv.sh
@@ -1,20 +1,13 @@
 #!/bin/sh
  ## Bash script for running several experiments
-fractions=(0.01 0.05 0.1 0.25 0.5 1.0)
+fractions=(0.1 0.25 0.5 1.0)
  # shellcheck disable=SC2068
  # shellcheck disable=SC1073
  # shellcheck disable=SC1061
 
  for fraction in "${fractions[@]}"
  do
-   python3 supervised_training_cleaned_cv.py --model_type deep_ecg_net --label_fraction $fraction --force_retraining --batch_size 64 --gpu 0 --force_retraining
- done
-
-fractions=(0.5 1.0)
-
- for fraction in "${fractions[@]}"
- do
-   python3 supervised_training_cleaned_cv.py --model_type transformer --gpu 0 --label_fraction $fraction --batch_size 64 --force_retraining
+   python3 supervised_training_cleaned_cv.py --dataset "stressid" --scoring_metric "f1" --fs 500 --model_type cnn --label_fraction $fraction --batch_size 64 --gpu 0 --force_retraining
  done
 
 
