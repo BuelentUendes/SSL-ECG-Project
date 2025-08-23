9c1d8d6cff8567b46ea9f577a7be0a4611dc5859
BuelentUendes <buelent.tt@web.de>
2025-08-23 14:50:47 +0200

    changed the dataset again to ours

--- training_pipelines/supervised_training_cleaned_cv.py
+++ training_pipelines/supervised_training_cleaned_cv.py
@@ -535,7 +535,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train ECG classifier")
     parser.add_argument("--mlflow_tracking_uri", default=os.getenv("MLFLOW_TRACKING_URI", "http://127.0.0.1:5000"))
     parser.add_argument("--fs", default=500, type=str, help="What sample frequency used for training")
-    parser.add_argument("--dataset", choices=("stressid", "wesad", "ours"), default="stressid", type=str)
+    parser.add_argument("--dataset", choices=("stressid", "wesad", "ours"), default="ours", type=str)
     parser.add_argument("--model_type",
                         choices=["cnn", "tcn", "transformer", "deep_ecg_net", "patchtst", "moment"], default="cnn")
     parser.add_argument("--label_fraction", type=float, default=0.25,
