f8625b6747f2232fdafa1af52874bb0f6ceb1be1
BuelentUendes <buelent.tt@web.de>
2025-08-25 16:04:52 +0200

    run seeds supervised

--- training_pipelines/train_supervised_cv.sh
+++ training_pipelines/train_supervised_cv.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
- ## Bash script for running several experiments
-fractions=(0.1 0.25 0.5 1.0)
  # shellcheck disable=SC2068
  # shellcheck disable=SC1073
  # shellcheck disable=SC1061
 
- for fraction in "${fractions[@]}"
- do
-   python3 supervised_training_cleaned_cv.py --dataset "stressid" --scoring_metric "f1" --fs 500 --model_type cnn --label_fraction $fraction --batch_size 64 --gpu 0 --force_retraining
- done
+ ## Bash script for running several experiments
+fractions=(0.01 0.05 0.1 0.25 0.5 1.0)
+seeds=(3 5 7 9)
+models=("cnn" "tcn" "transformer")
+
+for model in "${models[@]}"
+do
+  for seed in "${seeds[@]}"
+  do
+    for fraction in "${fractions[@]}"
+    do
+      python3 supervised_training_cleaned_cv.py --dataset "ours" --scoring_metric "roc_auc" --fs 1000 --model_type "$model" --seed "$seed" --label_fraction "$fraction" --batch_size 64 --gpu 0 --force_retraining
+    done
+  done
+done
+
 
 
