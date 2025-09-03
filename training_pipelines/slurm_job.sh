64547625a5744ba82cd4762ae3497d137eec59c1
BuelentUendes <buelent.tt@web.de>
2025-09-03 16:00:33 +0200

    simple test to run the script

--- training_pipelines/slurm_job.sh
+++ training_pipelines/slurm_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=ECG_SimCLR
-#SBATCH --time=2:00:00
+#SBATCH --time=0:10:00
 #SBATCH -N 1
 #SBATCH --gres=gpu:1
-#SBATCH --mem=16GB
+#SBATCH --mem=16G
 #SBATCH --output=ecg_%j.out
 #SBATCH --error=ecg_%j.err
 #SBATCH --mail-type=BEGIN,END
@@ -12,4 +12,4 @@
 module add cuda12.3/toolkit/12.3
 source activate /var/scratch/bun201/SSL-ECG-Project
 
-python3 simclr_train_cleaned_cv.py --seed 12345 --epochs 5
+python3 simclr_train_cleaned_cv.py --seed 12345 --epochs 3
