2d4d8bdd0629857416e225b91e409c49c470fe22
BuelentUendes <buelent.tt@web.de>
2025-09-03 16:13:12 +0200

    change the directoru

--- training_pipelines/slurm_job.sh
+++ training_pipelines/slurm_job.sh
@@ -5,10 +5,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --output=ecg_%j.out
 #SBATCH --error=ecg_%j.err
-#SBATCH --mail-type=BEGIN,END
-#SBATCH --mail-user=b.uendes@vu.nl
 
 module add cuda12.3/toolkit/12.3
 source activate /var/scratch/bun201/SSL-ECG-Project/ECG-Project
 
-python3 .training_pipelines/simclr_train_cleaned_cv.py --seed 12345 --epochs 2
+python3 simclr_train_cleaned_cv.py --seed 12345 --epochs 2
