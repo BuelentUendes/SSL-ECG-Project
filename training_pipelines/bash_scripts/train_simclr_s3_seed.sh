2d0f80f6e756eac3cc231719d53d2f51b8412fa7
BuelentUendes <buelent.tt@web.de>
2025-09-04 17:15:07 +0200

    added both versions s3 and plain vanilla simclr

---
+++ training_pipelines/bash_scripts/train_simclr_s3_seed.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+ # shellcheck disable=SC2068
+ # shellcheck disable=SC1073
+ # shellcheck disable=SC1061
+
+ ## Bash script for running several experiments
+seeds=(3 5 7 9 42)
+
+for seed in "${seeds[@]}"
+do
+  python3 simclr_train_cleaned_cv.py --force_retraining --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.1 --use_s3_layers
+  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.05 --use_s3_layers
+  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.01 --use_s3_layers
+  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.025 --use_s3_layers
+  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.25 --use_s3_layers
+  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.5 --use_s3_layers
+  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 1.0 --use_s3_layers
+done
+
