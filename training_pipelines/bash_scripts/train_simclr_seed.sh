09f248424db4ccb4239c5f11b842c7236d0aca60
BuelentUendes <buelent.tt@web.de>
2025-09-04 17:24:59 +0200

    fixed the scripts

--- training_pipelines/bash_scripts/train_simclr_seed.sh
+++ training_pipelines/bash_scripts/train_simclr_seed.sh
@@ -8,12 +8,12 @@ seeds=(3 5 7 9 42)
 
 for seed in "${seeds[@]}"
 do
-  python3 simclr_train_cleaned_cv.py --force_retraining --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.1 --use_s3_layers --gpu 0
-  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.05 --use_s3_layers --gpu 0
-  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.01 --use_s3_layers --gpu 0
-  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.025 --use_s3_layers --gpu 0
-  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.25 --use_s3_layers --gpu 0
-  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.5 --use_s3_layers --gpu 0
-  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 1.0 --use_s3_layers --gpu 0
+  python3 simclr_train_cleaned_cv.py --force_retraining --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.1 --gpu 0
+  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.05 --gpu 0
+  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.01 --gpu 0
+  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.025 --gpu 0
+  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.25 --gpu 0
+  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.5 --gpu 0
+  python3 simclr_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 1.0 --gpu 0
 done
 
