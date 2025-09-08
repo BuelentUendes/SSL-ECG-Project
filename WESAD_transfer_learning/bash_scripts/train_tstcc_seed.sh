865aed2c811bb3d29d892fb2ab99ae0514adf5ae
BuelentUendes <buelent.tt@web.de>
2025-09-08 20:10:16 +0200

    added the bash scripts

---
+++ WESAD_transfer_learning/bash_scripts/train_tstcc_seed.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+ # shellcheck disable=SC2068
+ # shellcheck disable=SC1073
+ # shellcheck disable=SC1061
+
+ ## Bash script for running several experiments
+fractions=(0.25 0.5 1.0)
+seeds=(3 5 7 9 42)
+
+for seed in "${seeds[@]}"
+do
+  python3 tstcc_train_cleaned_cv.py --force_retraining --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.25
+  python3 tstcc_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.5
+  python3 tstcc_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 1.0
+done
+
