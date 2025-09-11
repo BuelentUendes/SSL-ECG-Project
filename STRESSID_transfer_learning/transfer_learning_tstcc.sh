5b459f9d6e9a9935fd21af0925a4c0fda7f7cddc
BuelentUendes <buelent.tt@web.de>
2025-09-11 16:47:47 +0200

    added simple script to run through it via bash

---
+++ STRESSID_transfer_learning/transfer_learning_tstcc.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+ # shellcheck disable=SC2068
+ # shellcheck disable=SC1073
+ # shellcheck disable=SC1061
+
+ ## Bash script for running several experiments
+fractions=(0.1 0.25 0.5 1.0)
+seeds=(3 5 7 9 42) #3 5 7 9 42
+models=("logistic_regression")
+
+for model in "${models[@]}"
+do
+  for seed in "${seeds[@]}"
+  do
+    for fraction in "${fractions[@]}"
+    do
+      python3 tstcc_train_cleaned_cv.py  --fs 500 --use_pretrained_encoder --seed "$seed" --label_fraction "$fraction" --window_size 10 --step_size 5
+    done
+  done
+done
+
+
+
