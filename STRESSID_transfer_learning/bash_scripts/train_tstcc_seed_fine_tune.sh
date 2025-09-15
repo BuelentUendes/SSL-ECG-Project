6075f922f7c8d1839d36a78677c50324de53fcbf
BuelentUendes <buelent.tt@web.de>
2025-09-15 13:14:58 +0200

    added the fine tuning scripts for the bash scripts

---
+++ STRESSID_transfer_learning/bash_scripts/train_tstcc_seed_fine_tune.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+ # shellcheck disable=SC2068
+ # shellcheck disable=SC1073
+ # shellcheck disable=SC1061
+
+ ## Bash script for running several experiments
+fractions=(0.1 0.25 0.5 1.0)
+seeds=(3 5 7 9 42)
+
+for seed in "${seeds[@]}"
+do
+  python3 tstcc_train_cleaned_cv.py --use_pretrained_encoder --fine_tune_encoder --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.25 --fs 500
+  python3 tstcc_train_cleaned_cv.py --use_pretrained_encoder --fine_tune_encoder --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.1 --fs 500
+  python3 tstcc_train_cleaned_cv.py --use_pretrained_encoder --fine_tune_encoder --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.5 --fs 500
+  python3 tstcc_train_cleaned_cv.py --use_pretrained_encoder --fine_tune_encoder --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 1.0 --fs 500
+done
+
