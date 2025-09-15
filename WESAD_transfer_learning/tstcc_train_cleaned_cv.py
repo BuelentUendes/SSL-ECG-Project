4be58dbc71186d52e17a1b4503b55981fc067aca
BuelentUendes <buelent.tt@web.de>
2025-09-15 08:51:12 +0200

    added the save embeddings option now as well

--- WESAD_transfer_learning/tstcc_train_cleaned_cv.py
+++ WESAD_transfer_learning/tstcc_train_cleaned_cv.py
@@ -58,6 +58,7 @@ def main(
         tcc_batch_size: int,
         pretrain_all_conditions: bool,
         train_ratio_encoder: float,
+        save_embeddings: bool,
         tc_timesteps: int,
         tc_hidden_dim: int,
         cc_temperature: float,
@@ -157,8 +158,14 @@ def main(
         f"{seed}", f"{label_fraction}", f"{window_size}", f"{step_size}"
     )
 
+    # We will save the embeddings so we can later do some analysis on them
+    embedding_save_path = os.path.join(
+        DATA_PATH, "embeddings", "WESAD", f"{fs}", model_name, f"{seed}", f"{window_size}", f"{step_size}"
+    )
+
     create_directory(model_save_path)
     create_directory(results_save_path)
+    create_directory(embedding_save_path)
 
     # ── Step 1: Preprocess ───────────────────────────────────────────────────────
     if pretrain_all_conditions:
@@ -460,6 +467,11 @@ def main(
         test_repr, _ = encode_representations(X[test_idx], y[test_idx],
                                               model, tc_head, tcc_batch_size, device)
 
+        if save_embeddings:
+           x_repr_all, _ = encode_representations(X, y, model, tc_head, tcc_batch_size, device)
+           np.savez(os.path.join(embedding_save_path, "x_y_embedding.npz"), array1=x_repr_all, array_2=y)
+           print(f"We saved the embeddings and y")
+
     # filter to binary downstream samples
     train_repr = train_repr[downstream_mask["train"]]
     y_train = y[train_idx][downstream_mask["train"]]
@@ -592,6 +604,8 @@ if __name__ == "__main__":
     data_group.add_argument("--train_ratio_encoder", default=1.0, type=float,
                             help="If set to 0.75, it will result in 60/20/20 split and have a validation set for TSTCC,"
                                  "Alternatively, set to 1.0 to train on all unlabelled training instances.")
+    data_group.add_argument("--save_embeddings", action="store_true",
+                            help="If we want to save the embeddings. Note this is computationally expensive.")
     # ══════════════════════════════════════════════════════════════════════════════
     # TS-TCC Encoder Training
     # ══════════════════════════════════════════════════════════════════════════════
