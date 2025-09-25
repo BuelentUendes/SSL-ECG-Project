5ca8b838b58281ad48aa367d7424cf13e8ca3e09
BuelentUendes <buelent.tt@web.de>
2025-09-25 13:05:29 +0200

    simple script for transfer learning setup

---
+++ WESAD_transfer_learning/transfer_learning_tstcc.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+ # shellcheck disable=SC2068
+ # shellcheck disable=SC1073
+ # shellcheck disable=SC1061
+
+ ## Bash script for running several experiments
+fractions=(1.0)
+seeds=(3 5 7 9 42) #3 5 7 9 42
+models=("logistic_regression")
+
+for model in "${models[@]}"
+do
+  for seed in "${seeds[@]}"
+  do
+    for fraction in "${fractions[@]}"
+    do
+      python3 tstcc_train_cleaned_cv.py  --fs 700 --use_pretrained_encoder --fine_tune_encoder --seed "$seed" --label_fraction "$fraction" --window_size 10 --step_size 5
+      python3 tstcc_train_cleaned_cv.py  --fs 700 --use_pretrained_encoder --seed "$seed" --label_fraction "$fraction" --window_size 10 --step_size 5
+      # Now the S3 layers
+      python3 tstcc_train_cleaned_cv.py  --fs 700 --use_s3_layers --use_pretrained_encoder --fine_tune_encoder --seed "$seed" --label_fraction "$fraction" --window_size 10 --step_size 5
+      python3 tstcc_train_cleaned_cv.py  --fs 700 --use_s3_layers --use_pretrained_encoder --seed "$seed" --label_fraction "$fraction" --window_size 10 --step_size 5
+    done
+  done
+done
+
+
+
