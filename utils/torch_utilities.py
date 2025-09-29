1424dd42ae3c8a261a8ec954ba2804f435b6d995
BuelentUendes <buelent.tt@web.de>
2025-09-29 09:15:51 +0200

    updated the proper integer ones

--- utils/torch_utilities.py
+++ utils/torch_utilities.py
@@ -781,7 +781,7 @@ def standardize_features(X_train, X_val, X_test, feature_names):
     nn_indices = []
     standard_indices = []
 
-    min_max_scaler_names = ["nn20", "nk_pnn20", "nn50", "nk_pnn50"]
+    min_max_scaler_names = ["nn20","nn50", "wmax"] #These are the categorical ones
     for i, name in enumerate(feature_names):
         if name.lower() in min_max_scaler_names:
             nn_indices.append(i)
