e163ffabed21982ddff0ae6b2a26c3bc471ce92f
BuelentUendes <buelent.tt@web.de>
2026-02-03 15:12:18 +0100

    add the tcn for seed 42

--- training_pipelines/slurm_jobs/slurm_job_tcn.sh
+++ training_pipelines/slurm_jobs/slurm_job_tcn.sh
@@ -50,20 +50,20 @@ EOF
 echo "=== GPU Test Completed ==="
 # The first run runs it and retrains it for the specific seed
 python3 supervised_training_cleaned_cv.py --seed $1 --label_fraction 0.1 --window_size 10 --step_size 5 --model_type "tcn" --dataset "ours" --force_retraining
-#python3 supervised_training_cleaned_cv.py --seed $1 --label_fraction 0.01 --window_size 10 --step_size 5 --model_type "tcn" --dataset "ours"
-#python3 supervised_training_cleaned_cv.py --seed $1 --label_fraction 0.025 --window_size 10 --step_size 5 --model_type "tcn" --dataset "ours"
-#python3 supervised_training_cleaned_cv.py --seed $1 --label_fraction 0.05 --window_size 10 --step_size 5 --model_type "tcn" --dataset "ours"
-#python3 supervised_training_cleaned_cv.py --seed $1 --label_fraction 0.25 --window_size 10 --step_size 5 --model_type "tcn" --dataset "ours"
-#python3 supervised_training_cleaned_cv.py --seed $1 --label_fraction 0.5 --window_size 10 --step_size 5 --model_type "tcn" --dataset "ours"
-#python3 supervised_training_cleaned_cv.py --seed $1 --label_fraction 1.0 --window_size 10 --step_size 5 --model_type "tcn" --dataset "ours"
+python3 supervised_training_cleaned_cv.py --seed $1 --label_fraction 0.01 --window_size 10 --step_size 5 --model_type "tcn" --dataset "ours" --force_retraining
+python3 supervised_training_cleaned_cv.py --seed $1 --label_fraction 0.025 --window_size 10 --step_size 5 --model_type "tcn" --dataset "ours" --force_retraining
+python3 supervised_training_cleaned_cv.py --seed $1 --label_fraction 0.05 --window_size 10 --step_size 5 --model_type "tcn" --dataset "ours" --force_retraining
+python3 supervised_training_cleaned_cv.py --seed $1 --label_fraction 0.25 --window_size 10 --step_size 5 --model_type "tcn" --dataset "ours" --force_retraining
+python3 supervised_training_cleaned_cv.py --seed $1 --label_fraction 0.5 --window_size 10 --step_size 5 --model_type "tcn" --dataset "ours" --force_retraining
+python3 supervised_training_cleaned_cv.py --seed $1 --label_fraction 1.0 --window_size 10 --step_size 5 --model_type "tcn" --dataset "ours" --force_retraining
 
 # Command to run the job
-#  for SEED in 3 5 7 9 42; do
-#      sbatch --job-name=cnn_${SEED}_30_10 \
+#  for SEED in 42; do
+#      sbatch --job-name=tcn_${SEED}_10_5 \
 #             --output=ecg_tcn_10_5_${SEED}_%j.out \
 #             --error=ecg_tcn_10_5_${SEED}_%j.err \
-#             --begin=20:00 \
-#             --time=24:00:00 \
+#             --time=72:00:00 \
 #             ./slurm_jobs/slurm_job_tcn.sh $SEED
 #  done
 ##
+            # --begin=20:00 \
