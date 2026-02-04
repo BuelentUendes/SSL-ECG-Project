7066bc9fbf78923b1e17c53dcfd38847ba29aa85
BuelentUendes <buelent.tt@web.de>
2026-02-04 21:21:51 +0100

    tuning for info

--- training_pipelines/slurm_jobs/slurm_job_infots.sh
+++ training_pipelines/slurm_jobs/slurm_job_infots.sh
@@ -49,13 +49,13 @@ EOF
 
 echo "=== GPU Test Completed ==="
 # The first run runs it and retrains it for the specific seed
-python3 infots_train_cleaned_cv.py --seed $1 --infots_epochs 40 --label_fraction 0.1 --infots_batch_size 8 --force_retraining
-#python3 infots_train_cleaned_cv.py --seed $1 --infots_epochs 40 --label_fraction 0.01
-#python3 infots_train_cleaned_cv.py --seed $1 --infots_epochs 40 --label_fraction 0.025
-#python3 infots_train_cleaned_cv.py --seed $1 --infots_epochs 40 --label_fraction 0.05
-#python3 infots_train_cleaned_cv.py --seed $1 --infots_epochs 40 --label_fraction 0.25
-#python3 infots_train_cleaned_cv.py --seed $1 --infots_epochs 40 --label_fraction 0.5
-#python3 infots_train_cleaned_cv.py --seed $1 --infots_epochs 40 --label_fraction 1.0
+python3 infots_train_cleaned_cv.py --seed $1 --infots_epochs 40 --label_fraction 0.1 --force_retraining --infots_aug_p1 0.3
+python3 infots_train_cleaned_cv.py --seed $1 --infots_epochs 40 --label_fraction 0.01 --infots_aug_p1 0.3
+python3 infots_train_cleaned_cv.py --seed $1 --infots_epochs 40 --label_fraction 0.025 --infots_aug_p1 0.3
+python3 infots_train_cleaned_cv.py --seed $1 --infots_epochs 40 --label_fraction 0.05 --infots_aug_p1 0.3
+python3 infots_train_cleaned_cv.py --seed $1 --infots_epochs 40 --label_fraction 0.25 --infots_aug_p1 0.3
+python3 infots_train_cleaned_cv.py --seed $1 --infots_epochs 40 --label_fraction 0.5 --infots_aug_p1 0.3
+python3 infots_train_cleaned_cv.py --seed $1 --infots_epochs 40 --label_fraction 1.0 --infots_aug_p1 0.3
 
 #
 #  for SEED in 3 5 7 9 42; do
