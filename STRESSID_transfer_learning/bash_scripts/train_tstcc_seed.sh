17c587fba94e2acfe325737515681a10ab8991af
BuelentUendes <buelent.tt@web.de>
2026-08-11 09:36:53 +0200

    added the train from scratch for tstcc seeds on StressID

--- STRESSID_transfer_learning/bash_scripts/train_tstcc_seed.sh
+++ STRESSID_transfer_learning/bash_scripts/train_tstcc_seed.sh
@@ -4,14 +4,10 @@
  # shellcheck disable=SC1061
 
  ## Bash script for running several experiments
-fractions=(0.1 0.25 0.5 1.0)
-seeds=(3 5 7 9 42)
+seeds=(3 5 7 9 11 13 15 17 19 42)
 
 for seed in "${seeds[@]}"
 do
-  python3 tstcc_train_cleaned_cv.py --force_retraining --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.25 --fs 500
-  python3 tstcc_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.1 --fs 500
-  python3 tstcc_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.5 --fs 500
-  python3 tstcc_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 1.0 --fs 500
+  python3 tstcc_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 1.0 --fs 500 --force_retraining
 done
 
