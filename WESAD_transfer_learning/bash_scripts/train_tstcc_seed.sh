64b1442cc6c5f1ed2b4eda1ce2c3776780a89476
BuelentUendes <buelent.tt@web.de>
2026-08-11 09:34:47 +0200

    added the force retraining now

--- WESAD_transfer_learning/bash_scripts/train_tstcc_seed.sh
+++ WESAD_transfer_learning/bash_scripts/train_tstcc_seed.sh
@@ -9,6 +9,6 @@ seeds=(5 7 9 11 13 15 17 19 42) #Seed 3 was already run
 
 for seed in "${seeds[@]}"
 do
-  python3 tstcc_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 1.0 --fs 700
+  python3 tstcc_train_cleaned_cv.py --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 1.0 --fs 700 --force_retraining
 done
 
