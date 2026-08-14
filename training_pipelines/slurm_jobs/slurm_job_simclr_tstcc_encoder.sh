40e80c54eea779f335900a9455da2ba55a78aeb1
BuelentUendes <buelent.tt@web.de>
2026-08-14 12:05:29 +0200

    added the simclr tstcc endoder ablatio

--- training_pipelines/slurm_jobs/slurm_job_simclr_tstcc_encoder.sh
+++ training_pipelines/slurm_jobs/slurm_job_simclr_tstcc_encoder.sh
@@ -49,13 +49,13 @@ EOF
 
 echo "=== GPU Test Completed ==="
 # The first run runs it and retrains it for the specific seed
-python3 simclr_train_cleaned_cv.py --seed $1 --epochs 40 --label_fraction 0.1 --force_retraining --temperature 0.3 --use_tstcc_encoder
-python3 simclr_train_cleaned_cv.py --seed $1 --epochs 40 --label_fraction 0.01 --temperature 0.3 --use_tstcc_encoder
-python3 simclr_train_cleaned_cv.py --seed $1 --epochs 40 --label_fraction 0.025 --temperature 0.3 --use_tstcc_encoder
-python3 simclr_train_cleaned_cv.py --seed $1 --epochs 40 --label_fraction 0.05 --temperature 0.3 --use_tstcc_encoder
-python3 simclr_train_cleaned_cv.py --seed $1 --epochs 40 --label_fraction 0.25 --temperature 0.3 --use_tstcc_encoder
-python3 simclr_train_cleaned_cv.py --seed $1 --epochs 40 --label_fraction 0.5 --temperature 0.3 --use_tstcc_encoder
-python3 simclr_train_cleaned_cv.py --seed $1 --epochs 40 --label_fraction 1.0 --temperature 0.3 --use_tstcc_encoder
+python3 simclr_train_cleaned_cv.py --seed $1 --epochs 40 --label_fraction 0.1 --force_retraining --temperature 0.2 --use_tstcc_encoder
+python3 simclr_train_cleaned_cv.py --seed $1 --epochs 40 --label_fraction 0.01 --temperature 0.2 --use_tstcc_encoder
+python3 simclr_train_cleaned_cv.py --seed $1 --epochs 40 --label_fraction 0.025 --temperature 0.2 --use_tstcc_encoder
+python3 simclr_train_cleaned_cv.py --seed $1 --epochs 40 --label_fraction 0.05 --temperature 0.2 --use_tstcc_encoder
+python3 simclr_train_cleaned_cv.py --seed $1 --epochs 40 --label_fraction 0.25 --temperature 0.2 --use_tstcc_encoder
+python3 simclr_train_cleaned_cv.py --seed $1 --epochs 40 --label_fraction 0.5 --temperature 0.2 --use_tstcc_encoder
+python3 simclr_train_cleaned_cv.py --seed $1 --epochs 40 --label_fraction 1.0 --temperature 0.2 --use_tstcc_encoder
 
 # Command to run the job
 #  for SEED in 3 5 7 9 11 13 15 17 19 42; do
