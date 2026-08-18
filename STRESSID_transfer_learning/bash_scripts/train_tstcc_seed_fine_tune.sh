a79b8bb18b2699c5070ab9c1210fe4aa379ae5ca
BuelentUendes <buelent.tt@web.de>
2026-08-18 09:07:30 +0200

    fine tune lp ft for tstcc for stressid

--- STRESSID_transfer_learning/bash_scripts/train_tstcc_seed_fine_tune.sh
+++ STRESSID_transfer_learning/bash_scripts/train_tstcc_seed_fine_tune.sh
@@ -3,15 +3,13 @@
  # shellcheck disable=SC1073
  # shellcheck disable=SC1061
 
+# Script for LP fine-tuning for StressID
  ## Bash script for running several experiments
-fractions=(0.1 0.25 0.5 1.0)
-seeds=(3 5 7 9 42)
+fractions=(1.0)
+seeds=(3 5 7 9 11 13 15 17 19 42)
 
 for seed in "${seeds[@]}"
 do
-  python3 tstcc_train_cleaned_cv.py --use_pretrained_encoder --fine_tune_encoder --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.25 --fs 500
-  python3 tstcc_train_cleaned_cv.py --use_pretrained_encoder --fine_tune_encoder --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.1 --fs 500
-  python3 tstcc_train_cleaned_cv.py --use_pretrained_encoder --fine_tune_encoder --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 0.5 --fs 500
   python3 tstcc_train_cleaned_cv.py --use_pretrained_encoder --fine_tune_encoder --train_ratio_encoder 1.0 --seed "$seed" --label_fraction 1.0 --fs 500
 done
 
