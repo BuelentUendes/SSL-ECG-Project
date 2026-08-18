473fc67217a6c72f6a436071181a88496026c1a3
BuelentUendes <buelent.tt@web.de>
2026-08-18 09:16:58 +0200

    added the two wesad and stressid for cnn

--- training_pipelines/bash_scripts/train_supervised_cv_cnn_lp_ft_stressid.sh
+++ training_pipelines/bash_scripts/train_supervised_cv_cnn_lp_ft_stressid.sh
@@ -12,7 +12,6 @@ do
   for seed in "${seeds[@]}"
   do
     python3 supervised_training_cleaned_cv.py --dataset "stressid" --fs 500 --model_type "$model" --seed "$seed" --label_fraction 1.0 --batch_size 64 --use_pretrained_encoder --fine_tune_encoder --classifier_head "logistic_regression"
-    python3 supervised_training_cleaned_cv.py --dataset "wesad" --fs 700 --model_type "$model" --seed "$seed" --label_fraction 1.0 --batch_size 64 --use_pretrained_encoder --fine_tune_encoder --classifier_head "logistic_regression"
   done
 done
 
---
+++ training_pipelines/bash_scripts/train_supervised_cv_cnn_lp_ft_wesad.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+ # shellcheck disable=SC2068
+ # shellcheck disable=SC1073
+ # shellcheck disable=SC1061
+
+ ## Bash script for running several experiments
+seeds=(3 5 7 9 11 13 15 17 19 42)
+models=("cnn")
+
+for model in "${models[@]}"
+do
+  for seed in "${seeds[@]}"
+  do
+    python3 supervised_training_cleaned_cv.py --dataset "wesad" --fs 700 --model_type "$model" --seed "$seed" --label_fraction 1.0 --batch_size 64 --use_pretrained_encoder --fine_tune_encoder --classifier_head "logistic_regression"
+  done
+done
+
+
+
